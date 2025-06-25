170c0e1cebfbf438100556eace3f155c8589d45b
Tohubar <hossaintohubar@gmail.com>
2025-06-25 18:16:20 +0600

    3-auth handaled and route protected in client

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,97 +1,106 @@
-import User from "../models/user.model.js"
-import bcrypt from "bcryptjs"
-import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js"
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
 
 export const signup = async (req, res) => {
-    try {
-        const { fullName, username, email, password } = req.body
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        if (!emailRegex.test(email)) {
-            return res.status(400).json ({succes: false, messsage: "Invalid Email Format..."})
-        }
-        const existingUser = await User.findOne({ username }) // username: username => username
-        console.log("checkin useranaem")
-        if (existingUser) {
-            return res.status(400).json ({succes: false, messsage: "UserName is Already Taken."})
-        }
-        const existingEmail = await User.findOne({ email }) // username: username => username
-        console.log("checkin email")
-        if (existingEmail) {
-            return res.status(400).json ({succes: false, messsage: "Email is Already Taken."})
-        }
-        if (password.length < 6) {
-            return res.status(400).json({succes: false, messsage: "Password should be atleast 6 charactersf"})
-        }
-        //hashing password
-        const salt = await bcrypt.genSalt(10)
-        console.log("creating hash")
-        const hashedPassword = await bcrypt.hash(password, salt)
-        console.log("hased pass")
-        const newUser = new User ({
-            fullName,
-            username,
-            email,
-            password: hashedPassword,
-        })
-        if (newUser) {
-            generateTokenAndSetCookie(User._id, res)
-            await newUser.save()
+	try {
+		const { fullName, username, email, password } = req.body;
 
-            res.status(201).json ({
-                _id: newUser._id,
-                fullName: newUser.fullName,
-                username: newUser.username,
-                email: newUser.email,
-                followers:newUser.followers,
-                following: newUser.following,
-                profileImg: newUser.profileImg,
-                coverImg: newUser.coverImg
-            })
-        }else {
-            res.status(400).json({succes: false, messsage: "Invalid User data"})
-        }
-    } catch (error) {
-        res.status(500).json({succes: false, messsage: "Internal Server error"})
-    }
-}
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		if (!emailRegex.test(email)) {
+			return res.status(400).json({ success: false, message: "Invalid Email Format..." });
+		}
+
+		const existingUser = await User.findOne({ username });
+		if (existingUser) {
+			return res.status(400).json({ success: false, message: "Username is already taken." });
+		}
+
+		const existingEmail = await User.findOne({ email });
+		if (existingEmail) {
+			return res.status(400).json({ success: false, message: "Email is already taken." });
+		}
+
+		if (password.length < 6) {
+			return res.status(400).json({ success: false, message: "Password should be at least 6 characters." });
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const newUser = new User({
+			fullName,
+			username,
+			email,
+			password: hashedPassword,
+		});
+
+		if (newUser) {
+			await newUser.save();
+			generateTokenAndSetCookie(newUser._id, res);
+
+			res.status(201).json({
+				_id: newUser._id,
+				fullName: newUser.fullName,
+				username: newUser.username,
+				email: newUser.email,
+				followers: newUser.followers,
+				following: newUser.following,
+				profileImg: newUser.profileImg,
+				coverImg: newUser.coverImg,
+				success: true,
+			});
+		} else {
+			res.status(400).json({ success: false, message: "Invalid user data" });
+		}
+	} catch (error) {
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
+};
+
 export const login = async (req, res) => {
-    try {
-        const { username, password } = req.body
-        const user = await User.findOne({username})
-        const isValidPass = await bcrypt.compare(password, user? user.password : "") 
-        if (!user || !isValidPass) {
-            return res.status(400).json({succes: false, message: "Username or password is not correct"})
-        }
+	try {
+		const { username, password } = req.body;
 
-        generateTokenAndSetCookie(user._id, res)
-        res.status(201).json ({
-            _id: user._id,
-            fullName: user.fullName,
-            username: user.username,
-            email: user.email,
-            followers:user.followers,
-            following: user.following,
-            profileImg: user.profileImg,
-            coverImg: user.coverImg
-        })
-    
-    } catch (error) {
-        res.status(500).json({succes: false, messsage: "Internal Server error"})
-    }
-}
+		const user = await User.findOne({ username });
+		const isValidPass = await bcrypt.compare(password, user ? user.password : "");
+
+		if (!user || !isValidPass) {
+			return res.status(400).json({ success: false, message: "Username or password is not correct" });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(201).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			email: user.email,
+			followers: user.followers,
+			following: user.following,
+			profileImg: user.profileImg,
+			coverImg: user.coverImg,
+			success: true,
+		});
+	} catch (error) {
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
+};
+
 export const logout = async (req, res) => {
-    try {
-        res.cookie("jwt", "", {maxAge: 0})
-        res.status(200).json({succes: true, message: "Logout Successful"})
-    } catch (error) {
-        res.status(500).json({succes: false, messsage: "Internal Server error"})
-    }
-}
+	try {
+		res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ success: true, message: "Logout successful" });
+	} catch (error) {
+		res.status(500).json({ success: false, message: "Internal Server Error" });
+	}
+};
+
 export const getMe = async (req, res) => {
-    try {
-        const user = await User.findById(req.user._id).select("-password")
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(500).json({succes: false, messsage: "Internal Server error"})
-    }
-}
+	try {
+		const user = await User.findById(req.user._id).select("-password");
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(500).json({ success: false, error: "Internal Server Error" });
+	}
+};
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,56 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/home/HomePage'
-import SignUpPage from './pages/auth/signup/signUpPage'
-import LoginPage from './pages/auth/login/LoginPage'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import SignUpPage from './pages/auth/signup/signUpPage';
+import LoginPage from './pages/auth/login/LoginPage';
 import Sidebar from './components/common/SideBar';
 import RightPanel from './components/common/RightPanel';
 import NotificationPage from './components/notification/NotificationPage';
 import ProfilePage from './components/profile/ProfilePage';
+import { Toaster } from 'react-hot-toast';
+import { useQuery } from '@tanstack/react-query';
+import LoadingSpinner from './components/common/LoadingSpinner';
 
 const App = () => {
-  return (
-   <div className='flex max-w-6xl mx-auto'>
-    {/* //Common components bc these will showed always */}   
-      <Sidebar />
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/signup' element={<SignUpPage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/notifications' element={<NotificationPage />} />
-				<Route path='/profile/:username' element={<ProfilePage />} />
-			</Routes>
-      <RightPanel />
-		</div>
-  )
-}
+  const { data: authUser, isLoading, error, isError } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const res = await fetch ("/api/auth/me")
+        const data = await res.json()
+        if (data.error) return null
+        if (!res.ok) throw new Error(data.message || "Something went wrong")
+        
+        console.log("Auth User is here: ", data);
+        return data;
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+  })
+  if (isLoading) {
+    return (
+      <div className='h-screen flex justify-center items-center' >
+        <LoadingSpinner size='lg' />
+      </div>
+    )
+  }
 
-export default App
+  return (
+    <div className='flex max-w-6xl mx-auto'>
+      {authUser && <Sidebar />}
+      <div className='flex-1 px-4'>
+        <Routes>
+          <Route path='/' element={authUser? <HomePage /> : <Navigate to='/login' />} />
+          <Route path='/signup' element={!authUser? <SignUpPage /> : <Navigate to='/' />} />
+          <Route path='/login' element={!authUser? <LoginPage /> : <Navigate to='/' />} />
+          <Route path='/notifications' element={authUser? <NotificationPage /> : <Navigate to='/login' />} />
+          <Route path='/profile/:username' element={authUser? <ProfilePage /> : <Navigate to='/login' />} />
+        </Routes>
+      </div>
+      {authUser && <RightPanel />}
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
